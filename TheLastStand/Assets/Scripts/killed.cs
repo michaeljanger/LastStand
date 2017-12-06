baeb44047899b09ce6299a88b8b48a2c856dde76
michaeljanger <michaeljanger@gmail.com>
2017-12-05 23:29:15 -0800

    ragdoll

---
+++ TheLastStand/Assets/Scripts/killed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killed : MonoBehaviour {
+	public GameObject object2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision other){
+		Debug.Log ("col enemy");
+		if (other.gameObject.CompareTag("PlayerTag"))
+		{
+			Debug.Log ("killed enemy");
+			Instantiate(object2, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
